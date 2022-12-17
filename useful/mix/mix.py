74195d2dee75c9429fd48a069e55374c47303743
xiaoyo123 <36474975azl@gmail.com>
2022-12-17 19:17:10 +0800

    = =

---
+++ useful/mix/mix.py
@@ -0,0 +1,30 @@
+#取得資料夾所有圖片
+import os
+from PIL import Image
+def get_all_files(path):
+    files = []
+    for root, dirs, filenames in os.walk(path):
+        for f in filenames:
+            files.append(os.path.join(root, f))
+    return files
+
+
+res=get_all_files(".")
+all=[]
+for i in res:
+    if (i[-4:]!=".png") or (i[0:6]=="border"):
+        continue
+    all.append(i.replace(".\\",""))
+    
+print(all)
+imgSize = (1080, 1920)
+border=Image.open("border.png").convert("RGBA")
+border=border.resize(imgSize)
+for i in all:
+    if(i=="border.png"):
+        continue
+    img= Image.open(i)
+    img=img.resize(imgSize).convert("RGBA")
+    #將border.png和i的圖片合併
+    img=Image.alpha_composite(img,border)
+    img.save("fix\\"+i)
---
+++ useful/mix/rename.py
@@ -0,0 +1,20 @@
+import os
+def get_all_files(path):
+    files = []
+    for root, dirs, filenames in os.walk(path):
+        for f in filenames:
+            files.append(os.path.join(root, f).replace(".\\",""))
+    return files
+
+res=get_all_files(".")
+print(res)
+
+all=[]
+j=0
+for i in res:
+    
+    if(i[-3:]==".py") or (i=="border.png"):
+        continue
+    #將檔案重新命名
+    os.rename(i.replace(".\\",""),"def_"+str(j)+".png")
+    j+=1
