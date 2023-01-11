f8f16cbad1e36dd6ad15193ed9d060542a7af1f7
xiaoyo123 <36474975azl@gmail.com>
2023-01-11 22:05:18 +0800

    嚇死我了

--- page-php/login.php
+++ page-php/login.php
@@ -110,26 +110,27 @@
             <form action="sign_up.php" name=sign_up id="but">
                 <button id="sign">sign up</button>
             </form>
-        </div>
-        <?php
-        session_start();
-        if (isset($_POST['username'], $_POST['password'])) {
-            try {
-                $db = new PDO('mysql: host=localhost; dbname=account', 'root', '801559');
-                $stmt = $db->prepare('SELECT username FROM login WHERE username = ? and password = ?');
-                $stmt->execute(array($_POST['username'], $_POST['password']));
-                $row = $stmt->fetchAll();
-                if (count($row) == 0) {
-                    print "<span class='error'>wrong password or username</span>";
-                } else {
-                    $_SESSION['username'] = $_POST['username'];
-                    header("location: lobby.php");
+            <?php
+            session_start();
+            if (isset($_POST['username'], $_POST['password'])) {
+                try {
+                    $db = new PDO('mysql: host=localhost; dbname=account', 'root', '801559');
+                    $stmt = $db->prepare('SELECT username FROM login WHERE username = ? and password = ?');
+                    $stmt->execute(array($_POST['username'], $_POST['password']));
+                    $row = $stmt->fetchAll();
+                    if (count($row) == 0) {
+                        print "<span class='error'>wrong password or username</span>";
+                    } else {
+                        $_SESSION['username'] = $_POST['username'];
+                        header("location: lobby.php");
+                    }
+                } catch (PDOException $e) {
+                    print "error " . $e->getMessage();
                 }
-            } catch (PDOException $e) {
-                print "error " . $e->getMessage();
             }
-        }
-        ?>
+            ?>
+        </div>
+        
     </div>
 </body>
 
